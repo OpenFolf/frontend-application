97dccdee727b90b968ecd1c127cdd0614d851d20
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-06 06:26:58 +0000

    Auto stash before checking out "HEAD"

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-bottom-navigation app shift height="62" background-color="#41b883">
+    <v-btn x-large :to="{ name: 'home' }" color="#41b883">
+      <span class="mt-1">Home</span>
+      <v-icon>fa-home</v-icon>
+    </v-btn>
+
+    <v-btn x-large :to="{ name: 'new-game' }" color="#41b883">
+      <span class="mt-1">New Game</span>
+      <v-icon>fa-play</v-icon>
+    </v-btn>
+
+    <v-btn x-large :to="{ name: 'join-game' }" color="#41b883">
+      <span class="mt-1">Join Game</span>
+      <v-icon>fa-compress-alt</v-icon>
+    </v-btn>
+
+    <v-btn x-large :to="{ name: 'stats' }" color="#41b883">
+      <span class="mt-1">Stats</span>
+      <v-icon>fa-chart-bar</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    name: "bottom-nav",
+  };
+</script>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar color="#41b883" app>
+    <v-img
+      class="mr-2"
+      :src="require('@/assets/basket_white.png')"
+      max-height="40"
+      max-width="40"
+      contain
+    />
+    <v-toolbar-title>{{ user ? user.username : "" }}</v-toolbar-title>
+    <v-spacer />
+    <sign-out />
+  </v-app-bar>
+</template>
+
+<script>
+  import SignOut from "@/components/auth/SignOut.vue";
+  import { mapGetters } from "vuex";
+  export default {
+    name: "top-bar",
+    components: {
+      SignOut,
+    },
+    computed: {
+      ...mapGetters(["user"]),
+    },
+  };
+</script>
