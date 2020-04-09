3e7e23e7334de827cdbe346735e43c725e67ef40
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-09 22:32:03 +0000

    Total Router refactoring .

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Store from "../store";
 import Auth from "@/views/Auth.vue";
+import Home from "@/views/Home.vue";
+import HomeMenu from "@/components/home/Menu.vue";
 
 Vue.use(VueRouter);
 
@@ -9,11 +12,45 @@ const routes = [
     path: "/",
     name: "auth",
     component: Auth,
+    beforeEnter: (to, from, next) => {
+      if (Store.getters.getSignedIn) {
+        next({ name: "home-menu" });
+      } else {
+        next();
+      }
+    },
   },
+  // {
+  //   path: "/home",
+  //   name: "home",
+  // component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+  // },
   {
     path: "/home",
-    name: "home",
-    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+    // name: "home",
+    component: Home,
+    children: [
+      {
+        path: "",
+        name: "home-menu",
+        component: HomeMenu,
+      },
+      {
+        path: "/home/profile",
+        name: "home-profile",
+        component: () => import(/* webpackChunkName: "profile" */ "@/components/home/Profile.vue"),
+      },
+      {
+        path: "/home/privacy",
+        name: "home-privacy",
+        component: () => import(/* webpackChunkName: "privacy" */ "@/components/home/Privacy.vue"),
+      },
+      {
+        path: "/home/rules",
+        name: "home-rules",
+        component: () => import(/* webpackChunkName: "rules" */ "@/components/home/Rules.vue"),
+      },
+    ],
   },
   {
     path: "/game",
@@ -21,25 +58,36 @@ const routes = [
     component: () => import(/* webpackChunkName: "game" */ "@/views/Game.vue"),
   },
   {
-    path: "/game/lobby",
-    name: "lobby",
-    component: () => import(/* webpackChunkName: "lobby" */ "@/views/Lobby.vue"),
+    path: "/game/:path",
+    name: "game-course",
+    component: () => import(/* webpackChunkName: "game-course" */ "@/components/game/Course.vue"),
+    props: true,
   },
   {
-    path: "/game/scorecard",
-    name: "scorecard",
-    component: () => import(/* webpackChunkName: "scorecard" */ "@/views/Scorecard.vue"),
+    path: "/game/:path/lobby",
+    name: "game-lobby",
+    component: () => import(/* webpackChunkName: "lobby" */ "@/views/Lobby.vue"), // CHANGE LOCATION to COMPONENTS/GAME
+    props: true,
+  },
+  {
+    path: "/game/:path/scorecard",
+    name: "game-scorecard",
+    component: () => import(/* webpackChunkName: "scorecard" */ "@/views/Scorecard.vue"), // CHANGE LOCATION to COMPONENTS/GAME
+    props: true,
   },
   {
     path: "/join-game",
     name: "join-game",
     component: () => import(/* webpackChunkName: "join-game" */ "@/views/JoinGame.vue"),
   },
-
   {
     path: "/stats",
     name: "stats",
     component: () => import(/* webpackChunkName: "stats" */ "@/views/Stats.vue"),
+  },
+  {
+    path: "*",
+    redirect: { name: "auth" },
   },
 ];
 
@@ -50,24 +98,3 @@ const router = new VueRouter({
 });
 
 export default router;
-
-// EXAMPLE OF NESTED ROUTES IN VIEWPORT
-// {
-//   path: "/game",
-//   name: "game",
-//   component: () => import(/* webpackChunkName: "new-game" */ "@/views/NewGame.vue"),
-//   children: [
-//     {
-//       path: "lobby",
-//       name: "lobby",
-//       component: () => import(/* webpackChunkName: "lobby" */ "@/views/Lobby.vue"),
-//       // props: true,
-//     },
-//     {
-//       path: "scorecard",
-//       name: "scorecard",
-//       component: () => import(/* webpackChunkName: "join-game" */ "@/views/JoinGame.vue"),
-//       // props: true,
-//     },
-//   ],
-// },
