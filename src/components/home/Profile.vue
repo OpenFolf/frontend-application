3df09ee7fd200f3b268782847cffbd2151eac5e4
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-09 22:30:05 +0000

    Created subpages for home.

---
+++ src/components/home/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card flat color="transparent">
+    <v-container>
+      <v-row dense>
+        <v-col>
+          <h1>Profile</h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "home-profile",
+  };
+</script>
