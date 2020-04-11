f7a4cbf1a2c93e762502fd72081cf145db8155d4
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-11 06:04:55 +0000

    Working on profile

--- src/components/home/Profile.vue
+++ src/components/home/Profile.vue
@@ -1,17 +1,28 @@
 <template>
+  <!-- <v-card flat color="transparent">
+    <v-card-title>
+      Set Theme
+      <v-spacer />
+      <v-switch flat v-model="isDark" append-icon="fa-sun" prepend-icon="fa-moon" />
+    </v-card-title>
+    <v-card-actions>
+    </v-card-actions>
+  </v-card> -->
   <v-card flat color="transparent">
-    <v-container>
-      <v-row dense>
-        <v-col>
-          <h1>Profile</h1>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-text-field label="Display Name" solo flat height="5px" v-model="displyName" />
+    <v-text-field label="Initials" solo flat height="5px" v-model="displayInitials" />
+    Set Theme
+    <v-switch flat v-model="isDark" append-icon="fa-sun" prepend-icon="fa-moon" />
   </v-card>
 </template>
 
 <script>
   export default {
     name: "home-profile",
+    data() {
+      return {
+        toggle_multiple: [0, 1, 2],
+      };
+    },
   };
 </script>
