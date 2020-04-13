97e9b038f07291cbd0f592398390b8d954c2fd85
dagurdagur <34142077+dagurdagur@users.noreply.github.com>
2020-04-13 19:41:04 +0000

    create og get game

---
+++ src/graphql/custom/gamegraphQL.js
@@ -0,0 +1,77 @@
+// Queries, mutations and subscriptions for Game object
+
+export const createGame = /* GraphQL */ `
+  mutation CreateGame($input: CreateGameInput!, $condition: ModelGameConditionInput) {
+    createGame(input: $input, condition: $condition) {
+      id
+      owner {
+        id
+        username
+        email
+      }
+      course {
+        name
+      }
+      players {
+        items {
+          user {
+            username
+            email
+          }
+        }
+      }
+      gameStatus
+    }
+  }
+`;
+
+export const updateGame = /* GraphQL */ `
+  mutation UpdateGame($input: UpdateGameInput!, $condition: ModelGameConditionInput) {
+    updateGame(input: $input, condition: $condition) {
+      
+    }
+  }
+`;
+
+export const deleteGame = /* GraphQL */ `
+  mutation DeleteGame($input: DeleteGameInput!, $condition: ModelGameConditionInput) {
+    deleteGame(input: $input, condition: $condition) {
+      
+    }
+  }
+`;
+
+export const getGame = /* GraphQL */ `
+  query GetGame($id: ID!) {
+    getGame(id: $id) {
+      id
+      owner {
+        username
+        email
+      }
+      course {
+        name
+      }
+      players {
+        items {
+          user {
+            username
+            email
+          }
+          totalScore
+        }
+      }
+    }
+  }
+`;
+
+export const listGames = /* GraphQL */ `
+  query ListGames($filter: ModelGameFilterInput, $limit: Int, $nextToken: String) {
+    listGames(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        
+      }
+      nextToken
+    }
+  }
+`;
