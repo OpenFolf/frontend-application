04da22a72d8b41bd1c9003bc46178813d4316e52
Arnar <arnararnarson@gmail.com>
2020-04-19 22:53:29 +0000

    Added basic error handling and error boundary

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <v-app>
-    <router-view />
-    <bottom-nav v-if="getSignedIn && !getIsScorecard" />
+    <error-boundary>
+      <router-view />
+      <bottom-nav v-if="getSignedIn && !getIsScorecard" />
+    </error-boundary>
   </v-app>
 </template>
 
 <script>
   import { mapGetters } from "vuex";
   import BottomNav from "@/components/BottomNav.vue";
+  import ErrorBoundary from "../src/components/errorBoundary/error-boundary";
   export default {
     name: "App",
     components: {
       BottomNav,
+      ErrorBoundary,
     },
     computed: {
       ...mapGetters(["getSignedIn", "getIsScorecard"]),
