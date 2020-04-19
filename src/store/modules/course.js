4f5db19b377279768382ea4251296c036ad41caa
Arnar <arnararnarson@gmail.com>
2020-04-19 23:12:01 +0000

    Reverting changes to store files

--- src/store/modules/course.js
+++ src/store/modules/course.js
@@ -60,7 +60,7 @@ const actions = {
       context.commit("updateCourseList", courseList);
       //console.log("response", response);
     } catch (e) {
-      throw new TypeError("Error", e);
+      console.log("Error", e);
     }
   },
 
@@ -73,7 +73,7 @@ const actions = {
 
       context.commit("updateCurrentCourse", course);
     } catch (e) {
-      throw new TypeError("Error", e);
+      console.log("Error", e);
     }
   },
 
