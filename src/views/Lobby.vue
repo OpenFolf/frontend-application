fcd13c2ed1d140c489ef4036fabb95800256c75d
dagurdagur <34142077+dagurdagur@users.noreply.github.com>
2020-04-21 21:13:57 +0000

    delete player routing fix

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -151,7 +151,22 @@
           this.subscribeToGame();
         },
       },
-      getGameType() {},
+      getGameType() {
+        const gamePlayers = this.getGame.players.items;
+        const userId = this.getUser.id;
+        let playerInGame = false;
+        // Loop through and check if user is player in game
+        for (let i = 0; i < gamePlayers.length; i++) {
+          if (gamePlayers[i].id == userId) {
+            // If he is user in game then fetch game into state and set playerInGame bool to true
+            playerInGame = true;
+          }
+        }
+
+        if (!playerInGame) {
+          this.$router.push({ name: "home-menu" });
+        }
+      },
     },
   };
 </script>
