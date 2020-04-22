aceecd319c1b8daf78f9278beb0361c85e257ca6
Arnar <arnararnarson@gmail.com>
2020-04-22 21:09:10 +0000

    Cancel game button connected

--- src/components/game/ConfirmDialogue.vue
+++ src/components/game/ConfirmDialogue.vue
@@ -25,6 +25,13 @@
             {{ message.button2 }}
           </v-btn>
           <v-btn
+            v-if="message.title === 'Cancel Game'"
+            color="primary"
+            @click="$emit('cancelGame')"
+          >
+            {{ message.button2 }}
+          </v-btn>
+          <v-btn
             v-if="message.title === 'Kick Player'"
             color="primary"
             @click="$emit('removeUser', userToRemove)"
