ca5edad47e1033d4297c51a2dde8a88e90d200d4
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-24 18:40:53 +0000

    removed redundant dev code - refactor

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <error-boundary>
       <router-view />
       <bottom-nav v-if="signedIn && getBottomNav" />
-      <!-- <keyboard v-if="signedIn && !getBottomNav" /> -->
     </error-boundary>
   </v-app>
 </template>
@@ -11,13 +10,11 @@
 <script>
   import { mapGetters } from "vuex";
   import BottomNav from "@/components/BottomNav.vue";
-  // import Keyboard from "@/components/game/Keyboard.vue";
   import ErrorBoundary from "../src/components/errorBoundary/error-boundary";
   export default {
     name: "App",
     components: {
       BottomNav,
-      // Keyboard,
       ErrorBoundary,
     },
     computed: {
