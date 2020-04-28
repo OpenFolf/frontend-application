2b420d42b69ee39d55e81ce4b0158c2da911d3a1
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-28 03:34:22 +0000

    merging with master

--- src/components/home/Rules.vue
+++ src/components/home/Rules.vue
@@ -2,65 +2,71 @@
   <v-card color="transparent" flat>
     <v-container>
       <v-row dense>
-        <v-col>
-          <v-card-title>General Disc Golf Rules and Play</v-card-title>
-          <v-card-text
-            >Disc golf is played like ball golf using a flying disc. One point is counted each time
+        <v-col cols="12">
+          <v-container class="d-flex flex-row justify-center align-center">
+            <iframe frameborder="0" src="https://www.youtube.com/embed/tgbNymZ7vqY" />
+          </v-container>
+        </v-col>
+        <v-col cols="12">
+          <v-card-title class="font-weight-bold">General Disc Golf Rules</v-card-title>
+          <v-card-text>
+            Disc golf is played like ball golf using a flying disc. One point is counted each time
             the disc is thrown and when a penalty is incurred. The object is to acquire the lowest
-            score. Below you will find terms you will need to be familiar with.</v-card-text
-          >
-          <v-card-title>Tee Throws</v-card-title>
-          <v-card-text
-            >Tee throws must be completed within or behind the designated tee area. Do not throw
-            until the players in front of you are out of range.</v-card-text
-          >
-          <v-card-title>Lie</v-card-title>
-          <v-card-text
-            >The spot where the previous throw has landed, mark with a mini disc or leave the disc
-            as is. Your next throw is taken behind your lie on the ' line of play ' ,( an imaginary
-            line that runs from the target through the center of your marker). One foot must be on
-            the ' line of play ' at the time of release ( no more than 30 cm. behind ) and no
-            supporting point ( most often the other foot ) may be ahead of your lie at the time of
-            release.</v-card-text
-          >
-          <v-card-title>Throwing Order</v-card-title>
-          <v-card-text
-            >After teeing off, the player whose disc is farthest from the hole always throws first.
+            score. Below you will find terms you will need to be familiar with.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Tee Throws</v-card-title>
+          <v-card-text>
+            Tee throws must be completed within or behind the designated tee area. Do not throw
+            until the players in front of you are out of range.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Lie</v-card-title>
+          <v-card-text>
+            The spot where the previous throw has landed, mark with a mini disc or leave the disc as
+            is. Your next throw is taken behind your lie on the "line of play", an imaginary line
+            that runs from the target through the center of your marker. One foot must be on the
+            "line of play" at the time of release, no more than 30 cm. behind, and no supporting
+            point, most often the other foot, may be ahead of your lie at the time of release.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Throwing Order</v-card-title>
+          <v-card-text>
+            After teeing off, the player whose disc is farthest from the hole always throws first.
             The player with the least amount of throws on the previous hole is the first to tee off
-            on the next hole.</v-card-text
-          >
-          <v-card-title>Fairway Throws</v-card-title>
+            on the next hole.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Fairway Throws</v-card-title>
           <v-card-text>A run-up and follow-through, after release, is allowed.</v-card-text>
-          <v-card-title>Completion Of Hole</v-card-title>
-          <v-card-text
-            >A disc that comes to rest in the basket or chains constitutes the successful completion
+          <v-card-title class="font-weight-bold">Completion Of Hole</v-card-title>
+          <v-card-text>
+            A disc that comes to rest in the basket or chains constitutes the successful completion
             of that hole. A disc on top of the basket does not count as completion. As for tonals,
             once a disc hits the target, the hole is complete.
           </v-card-text>
-          <v-card-title>Un-Playable Lie</v-card-title>
-          <v-card-text
-            >Any disc that comes to rest above the ground is considered an un-playable lie. The disc
+          <v-card-title class="font-weight-bold">Un-Playable Lie</v-card-title>
+          <v-card-text>
+            Any disc that comes to rest above the ground is considered an un-playable lie. The disc
             must be thrown from the lie on the ground, directly underneath the un-playable lie. In
-            the case of excessive mud, puddles or to get out of harms way ( prickly or poisonous
-            plants, harmful insects etc), you may back up on the line of play, up to 5 metres to
-            establish your ' lie ' without penalty.</v-card-text
-          >
-          <v-card-title>Out Of Bounds</v-card-title>
-          <v-card-text
-            >A throw that lands out of bounds, must be played from a point 3 feet in bounds from
+            the case of excessive mud, puddles or to get out of harms way, prickly or poisonous
+            plants, harmful insects etc, you may back up on the line of play, up to 5 metres to
+            establish your ' lie ' without penalty.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Out Of Bounds</v-card-title>
+          <v-card-text>
+            A throw that lands out of bounds, must be played from a point 3 feet in bounds from
             where the disc went out of bounds or rethrown from the previous lie. Permanent water
             hazards and public roads are always out of bounds. Each course will have their own
-            specified out of bounds.</v-card-text
-          >
-          <v-card-title>Penalties</v-card-title>
-          <v-card-text
-            >There is no relief from obstacles that were present before you threw. A player may not
+            specified out of bounds.
+          </v-card-text>
+          <v-card-title class="font-weight-bold">Penalties</v-card-title>
+          <v-card-text>
+            There is no relief from obstacles that were present before you threw. A player may not
             move branches that are between his disc and the basket. (1 stroke penalty) If a player
             purposely moves or breaks any object between the disc and the hole a 2 stroke penalty is
             incurred. Otherwise recreational players will not be penalized for rule infractions.
-            Other players will keep you honest.</v-card-text
-          >
-          <v-card-subtitle>Source www.discgolfsherbrooke.com</v-card-subtitle>
+            Other players will keep you honest.
+          </v-card-text>
+          <v-card-subtitle class="font-weight-bold">
+            Source www.discgolfsherbrooke.com
+          </v-card-subtitle>
         </v-col>
       </v-row>
     </v-container>
