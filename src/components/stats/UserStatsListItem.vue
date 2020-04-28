6c24750c86d6c114fc35e4a197f4dc89a2fb06e4
Arnar <arnararnarson@gmail.com>
2020-04-28 02:05:29 +0000

    UI and code cleanup, fixed default scrolling of v-dialog

--- src/components/stats/UserStatsListItem.vue
+++ src/components/stats/UserStatsListItem.vue
@@ -1,49 +1,66 @@
 <template>
-  <v-dialog v-model="localModalVisible" overlay-opacity="0.7">
-    <v-card color="secondary" flat class="mb-2" @click="$emit('close')">
-      <!-- <v-card-title>{{ getUserGames[gameListIndex].id }} </v-card-title> -->
-      <v-card-title>{{ getUserGames[gameListIndex].course.courseName }}</v-card-title>
-      <v-card-subtitle>{{ getUserGames[gameListIndex].gameDate }}</v-card-subtitle>
-      <!-- 
-      <v-card-text v-for="players in gameItem" :key="players.id">
-        {{ players.user.email }}
-      </v-card-text> -->
-
+  <v-dialog v-model="dialog" width="400" overlay-opacity="0.7">
+    <template v-slot:activator="{ on }">
+      <v-card
+        v-on="on"
+        color="secondary"
+        flat
+        width="90%"
+        class="mt-3 mx-auto"
+        v-for="(gameList, gameListIndex) in getUserGames"
+        :key="gameList.id"
+        @click.stop="openModal(gameListIndex)"
+      >
+        <v-card-title class="addedColor white--text mb-4">
+          <h4>{{ gameList.course.courseName }} - {{ gameList.gameDate }}</h4>
+        </v-card-title>
+        <v-card-text
+          v-for="players in gameList.players"
+          :key="players.id"
+          class="d-flex justify-space-between bold"
+        >
+          <span class="text-left"> {{ players.user.email }} </span>
+          <span class="text-left"> Score: {{ players.totalScore }} </span>
+        </v-card-text>
+      </v-card>
+    </template>
+    <v-card color="secondary" flat class="mb-2" @click="dialog = false">
+      <v-card-title>{{ getUserGames[modalIndex].course.courseName }}</v-card-title>
+      <v-card-subtitle>{{ getUserGames[modalIndex].gameDate }}</v-card-subtitle>
       <v-simple-table class="mx-auto" dense>
         <template v-slot:default>
           <thead class="header" bold>
             <tr>
               <th class="title text-center">Hole</th>
               <th class="title text-center">Par</th>
-              <fragment v-for="teeName in gameItem" :key="teeName.id">
-                <th class="title text-center">{{ teeName.user.username }}</th>
-              </fragment>
+
+              <th v-for="teeName in gameItem" :key="teeName.id" class="title text-center">
+                {{ teeName.user.username }}
+              </th>
             </tr>
           </thead>
-
           <tbody>
             <tr v-for="(hole, holeIndex) in game.course.par" :key="holeIndex">
               <td class="text-center">{{ holeIndex }}</td>
               <td class="text-center">
                 {{ game.course.par[0].redPar }}
               </td>
-              <fragment v-for="(player, playerIndex) in gameItem" :key="playerIndex">
-                <td class="text-center">
-                  {{ player.scoreArray[holeIndex] }}
-                </td>
-              </fragment>
+              <td class="text-center" v-for="(player, playerIndex) in gameItem" :key="playerIndex">
+                {{ player.scoreArray[holeIndex] }}
+              </td>
             </tr>
           </tbody>
-
           <tfoot class="footer" bold>
             <tr>
               <th class="title text-center">Total:</th>
               <th class="title text-center">{{ game.course.totalPar }}</th>
-              <fragment v-for="(player, playerIndex) in gameItem" :key="playerIndex">
-                <th class="title text-center">
-                  {{ player.totalScore }}
-                </th>
-              </fragment>
+              <th
+                class="title text-center"
+                v-for="(player, playerIndex) in gameItem"
+                :key="playerIndex"
+              >
+                {{ player.totalScore }}
+              </th>
             </tr>
           </tfoot>
         </template>
@@ -53,19 +70,14 @@
 </template>
 
 <script>
-  import { Fragment } from "vue-fragment";
   import { mapGetters } from "vuex";
   export default {
     name: "user-stats-list-item",
-    props: {
-      gameListIndex: {
-        type: Number,
-        required: true,
-      },
-    },
+
     data() {
       return {
-        localModalVisible: true,
+        dialog: false,
+        modalIndex: 0,
       };
     },
     computed: {
@@ -74,16 +86,22 @@
         return this.getUserGames;
       },
       game() {
-        return this.getUserGames[this.gameListIndex];
+        return this.getUserGames[this.modalIndex];
       },
       gameItem() {
-        return this.getUserGames[this.gameListIndex].players;
+        return this.getUserGames[this.modalIndex].players;
       },
     },
-    components: {
-      Fragment,
+    methods: {
+      openModal(gameListIndex) {
+        this.modalIndex = gameListIndex;
+      },
     },
-    methods: {},
+    watch: {
+      preventDialogFromScrollingToTop() {
+        document.getElementsByClassName("v-dialog--active")[0].scrollTop = 0;
+      },
+    },
   };
 </script>
 
